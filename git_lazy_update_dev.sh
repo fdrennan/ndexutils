b370cb9661868368f3b1e2ee7d4b4dc96a6a4a13
Freddy Drennan <drennanfreddy@gmail.com>
2020-09-22 18:26:07 -0600

    Lazy update

--- git_lazy_update_dev.sh
+++ git_lazy_update_dev.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-git checkout dev
 git add --all
 git commit -m 'Lazy update'
 git reset --hard
 git push origin dev
 git checkout beta
+git reset --hard
 git pull origin dev
 git reset --hard
 git push origin beta
 git checkout master
+git reset --hard
 git pull origin beta
 git push origin master
-git reset --hard
